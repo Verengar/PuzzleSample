ae92e0434e64cef4b342c1bba154312cf4ca5ead
Admin <admin@Admins-Mac.local>
2017-09-05 00:16:53 +0300

    Initial Commit

---
+++ PuzzleSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PuzzleSample
+//
+//  Created by Admin on 05.09.2017.
+//  Copyright © 2017 Łukasz Stoiński. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
